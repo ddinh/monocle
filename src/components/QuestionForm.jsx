5cbb8432e5db88fe39c8acd8cd64cb96263fce67
Derek <ddinh@users.noreply.github.com>
2018-10-20 13:49:35 -0500

    Add basic question creating functionality

---
+++ src/components/QuestionForm.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+
+export default class QuestionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: '',
+      type: 1,
+      numChoices: 4,
+      choices: {}
+    };
+  }
+
+  handleSelectType = e => {
+    this.setState({
+      type: e.target.value
+    });
+  };
+
+  handleChangeNumChoices = e => {
+    if (e.currentTarget.value) {
+      this.setState({
+        ...this.state,
+        numChoices: parseInt(e.currentTarget.value)
+      });
+    }
+  };
+
+  handleChoiceChange = (i) => (e) => {
+    const choices = this.state.choices;
+    choices[i] = e.target.value;
+
+    this.setState({
+      ...this.state,
+      choices: choices
+    })
+  }
+
+  handleQuestionChange = (e) => {
+    this.setState({
+      ...this.state,
+      question: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const data = this.state;
+    data.choices = Object.values(data.choices)
+
+    const payload = {
+      type: "QuestionPOST",
+      data: JSON.stringify(data)
+    }
+
+    console.log(payload);
+  };
+
+  render() {
+    const { question } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="">Question</label>
+            <input
+              type="text"
+              value={question}
+              onChange={this.handleQuestionChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="type">Type</label>
+            <select onChange={this.handleSelectType}>
+              <option value="1">Multiple Choice</option>
+              <option value="0">Short Answer</option>
+            </select>
+          </div>
+
+          {this.renderMultipleChoiceFields()}
+
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+
+  renderMultipleChoiceFields = () => {
+    const { type, numChoices } = this.state;
+
+    if (type == 1) {
+      return (
+        <div>
+          <div>
+            <label htmlFor="">Number of choices</label>
+            <input
+              type="number"
+              value={numChoices}
+              onChange={this.handleChangeNumChoices}
+            />
+
+            {[...Array(numChoices).keys()].map(i => this.renderChoiceField(i))}
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  };
+
+  renderChoiceField(number) {
+    return (
+      <div>
+        <label htmlFor="">{number}</label>
+        <input type="text" onChange={this.handleChoiceChange(number)}/>
+      </div>
+    );
+  }
+}
---
+++ src/pages/QuestionPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import QuestionForm from '../components/QuestionForm';
+
+export default class QuestionPage extends React.Component {
+  render() {
+    return <QuestionForm />
+  }
+}
